aceffa54e14a414c82e12459b4767f6bae342c65
Dekroo <dekroostream@gmail.com>
2025-06-28 16:38:43 +0200

    Zxing BarcodeScanner

---
+++ app/src/main/java/com/example/qrcodescanner/Zxing.kt
@@ -0,0 +1,63 @@
+package com.example.qrcodescanner
+
+import android.util.Log
+import androidx.annotation.OptIn
+import androidx.camera.core.ExperimentalGetImage
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.barcode.BarcodeScanner
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.ZoomSuggestionOptions
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
+
+fun ZXing() {
+    val options = BarcodeScannerOptions.Builder()
+        .setBarcodeFormats(
+            Barcode.FORMAT_QR_CODE,
+            Barcode.FORMAT_AZTEC
+        )
+        .build()
+}
+
+class ImageAnalyzer(
+    private val onBarcodeDetected: (String) -> Unit
+) : ImageAnalysis.Analyzer {
+        private val scanner  = BarcodeScanning.getClient(
+            BarcodeScannerOptions.Builder()
+                .setBarcodeFormats(
+                    Barcode.FORMAT_QR_CODE,
+                    Barcode.FORMAT_AZTEC
+                    )
+                .build()
+        )
+
+    @OptIn(ExperimentalGetImage::class)
+    override fun analyze(imageProxy: ImageProxy) {
+        val mediaImage = imageProxy.image
+        if (mediaImage != null) {
+            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+
+            scanner.process(image)
+                .addOnSuccessListener { barcodes ->
+                    for (barcode in barcodes) {
+                        val rawValue = barcode.rawValue //qr code text
+                        if (rawValue != null) {
+                            onBarcodeDetected(rawValue)
+                        }
+                    }
+                }
+                .addOnFailureListener {
+                    Log.d("ImageAnalyzer", "Scanner failed")
+                }
+                .addOnCompleteListener {
+                    imageProxy.close()
+                }
+        } else {
+            Log.d("ImageAnalyzer", "MediaImage null")
+            imageProxy.close()
+        }
+    }
+}
+
