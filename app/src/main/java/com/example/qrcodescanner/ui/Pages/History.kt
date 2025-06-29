ffc40f46c37224f581414c0a326e41564205adf2
Dekroo <dekroostream@gmail.com>
2025-06-29 17:14:52 +0200

    Create History.kt

---
+++ app/src/main/java/com/example/qrcodescanner/ui/Pages/History.kt
@@ -0,0 +1,20 @@
+package com.example.qrcodescanner.ui.Pages
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun HistoryPage() {
+    Text(
+        text = "xd"
+    )
+}
+
+@Composable
+@Preview
+fun HistoryPagePreview() {
+    HistoryPage()
+}
